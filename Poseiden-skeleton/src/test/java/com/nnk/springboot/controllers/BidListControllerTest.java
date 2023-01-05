0fbf89fe253e51c17a9cb4f37817cd325dc85078
Daniele410 <miraglia.daniele410@gmail.com>
2023-01-05 13:13:03 +0100

    add BidListControllerTest

--- Poseiden-skeleton/src/test/java/com/nnk/springboot/controllers/BidListControllerTest.java
+++ Poseiden-skeleton/src/test/java/com/nnk/springboot/controllers/BidListControllerTest.java
@@ -1,5 +1,6 @@
 package com.nnk.springboot.controllers;
 
+import com.nimbusds.jose.shaded.json.JSONObject;
 import com.nnk.springboot.domain.BidList;
 import com.nnk.springboot.service.impl.BidListServiceImpl;
 import com.nnk.springboot.service.impl.UserDetailServiceImpl;
@@ -14,6 +15,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -25,13 +27,18 @@ import org.springframework.web.servlet.ModelAndView;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
+import static org.hamcrest.Matchers.containsString;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+@ActiveProfiles("test")
 @ExtendWith(SpringExtension.class)
 @WebMvcTest(BidListController.class)
 class BidListControllerTest {
@@ -55,7 +62,7 @@ class BidListControllerTest {
 
     @Test
     void showBidListShouldReturnModifiedBidListView() throws Exception {
-       //Given
+        //Given
         BidList bid = new BidList("Account", "Type", 5d);
 
         when(bidService.findAll()).thenReturn(Arrays.asList(bid));
@@ -71,20 +78,156 @@ class BidListControllerTest {
     }
 
     @Test
-    void addBidForm() {
+    void addBidForm() throws Exception {
+
+        //Given
+        BidList bid = new BidList("Account", "Type", 5d);
+
+        when(bidService.findAll()).thenReturn(Arrays.asList(bid));
+        //When
+        mockMvc.perform(get("/bidList/add")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                //Then
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(model().attributeExists("bidList"))
+                .andExpect(view().name("bidList/add"));
+
+
     }
 
     @Test
-    void validate() {
+    void validateAddBid_shouldReturnError() throws Exception {
+
+        //Given
+        BidList bid = new BidList("Account", "Type", 5d);
+
+        //GIVEN
+        JSONObject json = new JSONObject();
+        json.put("account", "toto");
+        json.put("type", "tutu");
+        json.put("bidQuantity", 20);
+
+        when(bidService.save(any())).thenReturn(bid);
+        //When
+        mockMvc.perform(post("/bidList/validate")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(json.toString())
+                        .accept(MediaType.APPLICATION_JSON))
+                //Then
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(model().attributeExists("bidList"))
+                .andExpect(view().name("/bidList/add"));
+
     }
 
     @Test
-    void showUpdateForm() {
+    void validateAddBid_shouldReturnModifiedBidListView() throws Exception {
+
+        //Given
+        BidList bid = new BidList("Account", "Type", 5d);
+
+        when(bidService.update(any())).thenReturn(bid);
+
+        //When
+        mockMvc.perform(post("/bidList/validate")
+                        .sessionAttr("bidList", bid)
+                        .param("account", bid.getAccount())
+                        .param("type", bid.getType())
+                        .param("bidQuantity", "1.0")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+
+                //Then
+                .andDo(print())
+                .andExpect(view().name("redirect:/bidList/list"));
+
     }
 
     @Test
-    void updateBid() {
+    void showUpdateForm_shouldReturnModifiedBidListView() throws Exception {
+
+        //Given
+        BidList bid = new BidList("Account", "Type", 5d);
+
+        when(bidService.findById(any())).thenReturn(Optional.of(bid));
+        //When
+        mockMvc.perform(get("/bidList/update/{id}",1)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                //Then
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(model().attributeExists("bidList"))
+                .andExpect(view().name("bidList/update"));
     }
+
+    @Test
+    void updateBid_shouldReturnModifiedBidListView() throws Exception {
+        //Given
+        BidList bid = new BidList("Account", "Type", 5d);
+        bid.setBidListId(1);
+        when(bidService.update(any())).thenReturn(bid);
+
+        //When
+        mockMvc.perform(post("/bidList/validate")
+                .sessionAttr("bidList", bid)
+                .param("account", bid.getAccount())
+                .param("type", bid.getType())
+                .param("bidQuantity", "1.0")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON));
+
+        mockMvc.perform(post("/bidList/update/1")
+                        .sessionAttr("bidList", bid)
+                        .param("account", "NewAccount")
+                        .param("type", bid.getType())
+                        .param("bidQuantity", "1.0")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+
+                //Then
+                .andDo(print())
+                .andExpect(status().isFound())
+                .andExpect(view().name("redirect:/bidList/list"));
+
+    }
+
+    @Test
+    void updateBid_shouldReturnErrorInViewBidList() throws Exception {
+//        //Given
+        BidList bid = new BidList("Account", "Type", 5d);
+//        bid.setBidListId(1);
+////        when(bidService.update(any())).thenReturn(bid);
+//
+//        //When
+//
+//        mockMvc.perform(post("/bidList/update/01")
+//                        .sessionAttr("bidList", bid)
+//                        .contentType(MediaType.APPLICATION_JSON)
+//                        .accept(MediaType.APPLICATION_JSON))
+//
+//                //Then
+//                .andDo(print())
+//                .andExpect(status().isFound())
+//                .andExpect(view().name("bidList/update"));
+//        //ARRANGE
+//        when(bidService.save(bid)).thenReturn(bid);
+//        //ACT
+//        mockMvc.perform(post("/bidList/update/1")
+//                        .sessionAttr("bidList", bid)
+//                        .param("account", bid.getAccount())
+//                        .param("type", bid.getType())
+//                        .param("bidQuantity", "0")
+//                        .contentType(MediaType.APPLICATION_JSON)
+//                        .accept(MediaType.APPLICATION_JSON))
+//                //ASSERT
+//                .andDo(print())
+//                .andExpect(view().name("bidList/update"));
+    }
+
 
     @Test
     void deleteBid() {
